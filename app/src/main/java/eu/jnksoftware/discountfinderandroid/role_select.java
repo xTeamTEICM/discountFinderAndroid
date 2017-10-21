3b1a6278b9f53f872c21b57a96c749fb5d3b00dc
kostasmantz <kostiksm@gmail.com>
2017-10-21 18:38:42 +0300

    commit for issue #4

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/role_select.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/role_select.java
@@ -2,6 +2,8 @@ package eu.jnksoftware.discountfinderandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class role_select extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class role_select extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role_select);
+
+        TextView user_register = (TextView) findViewById(R.id.tvUserRegister);
+        TextView shop_register = (TextView) findViewById(R.id.tvShopRegister);
+
+        user_register.setOnClickListener(userRegClick);
+        shop_register.setOnClickListener(shopRegClick);
     }
+
+    private View.OnClickListener userRegClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //TODO: link with user register
+        }
+    };
+
+    private View.OnClickListener shopRegClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //TODO:link with shop register
+        }
+    };
 }
