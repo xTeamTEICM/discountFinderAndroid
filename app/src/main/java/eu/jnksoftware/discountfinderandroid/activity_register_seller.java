b861292a2a87ce6af8a83805f0ba9deb9a12a181
kostasmantz <kostiksm@gmail.com>
2017-10-21 15:28:06 +0300

    commit for issue #3

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/activity_register_seller.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/activity_register_seller.java
@@ -1,13 +1,15 @@
 package eu.jnksoftware.discountfinderandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class activity_register_seller extends AppCompatActivity {
+public class activity_register_seller extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register_seller);
     }
 }
