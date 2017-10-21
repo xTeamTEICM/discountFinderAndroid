21562783a86f0f27f73366de034864d7339d335e
kostasmantz <kostiksm@gmail.com>
2017-10-21 16:38:00 +0300

    done issue #3

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/activity_register_seller.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/activity_register_seller.java
@@ -2,14 +2,35 @@ package eu.jnksoftware.discountfinderandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class activity_register_seller extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register_seller);
+
+        EditText etName = (EditText) findViewById(R.id.etName);
+        EditText etSurname = (EditText) findViewById(R.id.etSurname);
+        EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        EditText etPass = (EditText) findViewById(R.id.etPassword);
+        EditText etBrand = (EditText) findViewById(R.id.etBrand);
+
+        Button register = (Button) findViewById(R.id.btRegister);
+        register.setOnClickListener(registerClick);
     }
+
+    private View.OnClickListener registerClick = new View.OnClickListener(){
+        @Override
+        public void onClick(final View r){
+            //TODO: Perform Register
+        }
+    };
+
 }
