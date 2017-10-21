da6b27e4a5282583caf2f729c57a42af71eecf8e
kostasmantz <kostiksm@gmail.com>
2017-10-21 17:52:27 +0300

    commit for issue #4

---
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/role_select.java
@@ -0,0 +1,13 @@
+package eu.jnksoftware.discountfinderandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class role_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role_select);
+    }
+}
