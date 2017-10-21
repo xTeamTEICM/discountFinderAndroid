1e189813d16368804a6cb167d6224cf1db288568
Delis Nikos <delisnikos95@gmail.com>
2017-10-21 13:58:04 +0300

    Login UI changes
    Insert Login Button Handler and  registerView a link

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/loginActivity.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/loginActivity.java
@@ -3,7 +3,11 @@ package eu.jnksoftware.discountfinderandroid;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class loginActivity extends Activity {
 
@@ -12,5 +16,18 @@ public class loginActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        Button login=(Button) findViewById(R.id.login_btn);
+        login.setOnClickListener(loginClick);
+
+        TextView registerView=(TextView) findViewById(R.id.registerTextView);
+        registerView.setMovementMethod(LinkMovementMethod.getInstance());
     }
+    private View.OnClickListener loginClick=new View.OnClickListener(){
+        @Override
+        public void onClick(final View loginview) {
+            //ToDo:Login to main page
+
+        }
+    };
 }
