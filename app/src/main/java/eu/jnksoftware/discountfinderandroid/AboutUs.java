2acd360200f8c93864eedb039f24c6cae48a461f
ArisPoz <aris.pozidis96@gmail.com>
2017-10-21 19:42:10 +0300

    About Us (Gets version from Jenkins)

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/AboutUs.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/AboutUs.java
@@ -26,16 +26,33 @@ public class AboutUs extends AppCompatActivity {
                 .addPlayStore(BuildConfig.APPLICATION_ID)
                 .addEmail("xteamteicm@gmail.com")
                 .addGroup("Third Party Libraries")
+                .addItem(usedLibraries())
                 .addItem(createCopyright())
                 .create();
         setContentView(aboutUs);
+    }
+
+    private Element usedLibraries() {
+        Element library = new Element();
+        final String libraryString = "com.github.medyo:android-about-page:1.1.1";
+        library.setIcon(R.drawable.icon3);
+        library.setTitle("Medyo About Page");
+        library.setGravity(Gravity.START);
+        library.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AboutUs.this, libraryString, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return library;
     }
 
     private Element createCopyright() {
         Element copyright = new Element();
         final String copyrightString = "Copyright 2017 by x-Team";
         copyright.setIcon(R.drawable.icon2);
-        copyright.setGravity(Gravity.CENTER);
+        copyright.setTitle("Copyright Alright Reserved");
+        copyright.setGravity(Gravity.START);
         copyright.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
