ec3409575131f8ac215f4f57b11e0ff1868f9e3b
sotikele <xsotos@hotmail.com>
2017-10-22 21:15:26 +0300

    About #9 open from users device GoogleMap application from ShopsList

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/Shop.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/Shop.java
@@ -1,5 +1,7 @@
 package eu.jnksoftware.discountfinderandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,6 +15,27 @@ public class Shop extends AppCompatActivity {
         this.shopName = shopName;
         this.location = location;
         this.distanceFromUser = distanceFromUser;
+    }
+
+    public Intent openOnMaps(){
+
+        Double Latitude = location.getX();
+        Double Longitude = location.getY();
+        String labelLocation = "x-Team Sample : " + shopName;
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+
+        intent.setData(Uri.parse("geo:<" + Latitude  + ">,<" + Longitude + ">?q=<" + Latitude  + ">,<" + Longitude + ">(" + labelLocation+ ")"));
+
+        //if user has not the application googleMaps it will show him a dialog with Launch Maps
+        Intent chooser=Intent.createChooser(intent,"Launch Maps");
+        return chooser;
+
+
+    }
+    // this toString prints on ListView
+    public String toString(){
+
+        return this.shopName;
     }
 
 }
