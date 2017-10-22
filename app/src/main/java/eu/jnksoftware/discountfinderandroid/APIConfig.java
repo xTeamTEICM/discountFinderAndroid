395a7b8ed300b08842b9872a5028dbb3199e4366
Jordan Kostelidis <JordanKostelidis@JNKSoftware.eu>
2017-10-22 21:45:05 +0300

    APIConfig File
    Links for production API and testing API

---
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/APIConfig.java
@@ -0,0 +1,13 @@
+package eu.jnksoftware.discountfinderandroid;
+
+/**
+ * Owner: JNK Software
+ * Developer: Jordan Kostelidis
+ * Date: 22/10/2017
+ * License: Apache License 2.0
+ */
+public class APIConfig {
+    public final static String APILinkReal = "http://ouranos.jnksoftware.eu/discountFinderBackend/API/";
+    public final static String APILinkTest = "https://ouranos.jnksoftware.eu/testUncommited/API/";
+
+}
