ff67d3acf8248c3b8dea8d40278107e0c9355019
kostasmantz <kostiksm@gmail.com>
2017-10-22 13:08:46 +0300

    close #4

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/RegisterRoleSelectorUI.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/RegisterRoleSelectorUI.java
@@ -1,15 +1,18 @@
 package eu.jnksoftware.discountfinderandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
-public class role_select extends AppCompatActivity {
+public class RegisterRoleSelectorUI extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_role_select);
 
         TextView user_register = (TextView) findViewById(R.id.tvUserRegister);
