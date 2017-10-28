6be33f974f7a5762020fcf3f6db6c8a4387e160e
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-10-28 20:08:00 +0300

    User Class completed with testing

---
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/models/Role.java
@@ -0,0 +1,13 @@
+package eu.jnksoftware.discountfinderandroid.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by makis on 26/10/2017.
+ */
+
+public enum Role implements Serializable {
+    SELLER,
+    CUSTOMER;
+
+}
---
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/models/User.java
@@ -0,0 +1,95 @@
+package eu.jnksoftware.discountfinderandroid.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import eu.jnksoftware.discountfinderandroid.services.BinaryIO;
+
+/**
+ * Created by makis on 26/10/2017.
+ */
+
+public class User implements Serializable {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    private Date creationDate;
+    private Date lastLogin;
+    private String token;
+
+    public User(int ID, String firstName, String lastName, String email, Role role, Date creationDate, Date lastLogin, String token) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.creationDate = creationDate;
+        this.lastLogin = lastLogin;
+        this.token = token;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Date getCreationDate() {return creationDate;}
+
+    public void setCreationDate(Date creationDate) {
+
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
