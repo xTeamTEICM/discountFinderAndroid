c3a51db73ab89ec0fde4ce47abf5f89fc6cd5609
kostasmantz <kostiksm@gmail.com>
2017-11-02 01:45:02 +0200

    commit for issue #71 add the spinner

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/ui/customer/Add_Discount.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/ui/customer/Add_Discount.java
@@ -2,6 +2,8 @@ package eu.jnksoftware.discountfinderandroid.ui.customer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import eu.jnksoftware.discountfinderandroid.R;
 
@@ -11,5 +13,11 @@ public class Add_Discount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__discount);
+
+        Spinner spinnerCat = (Spinner) findViewById(R.id.spinnerCategory);
+        ArrayAdapter<String> spinContentAdapter = new ArrayAdapter<String>(Add_Discount.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.categories));
+
+        spinContentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCat.setAdapter(spinContentAdapter);
     }
 }
