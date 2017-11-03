70ad0b952a8763b5515463bc4a39ad3dbdf8a2c6
kostasmantz <kostiksm@gmail.com>
2017-11-03 16:19:09 +0200

    commit for issue #71

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/ui/customer/Add_Discount.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/ui/customer/Add_Discount.java
@@ -3,6 +3,7 @@ package eu.jnksoftware.discountfinderandroid.ui.customer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 
 import eu.jnksoftware.discountfinderandroid.R;
@@ -16,6 +17,11 @@ public class Add_Discount extends AppCompatActivity {
 
         Spinner spinnerCat = (Spinner) findViewById(R.id.spinnerCategory);
         ArrayAdapter<String> spinContentAdapter = new ArrayAdapter<String>(Add_Discount.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.categories));
+        SeekBar seekbarPrice = (SeekBar) findViewById(R.id.seekBarPrice);
+
+        seekbarPrice.getBackground().setAlpha(255);
+        spinnerCat.getBackground().setAlpha(130);
+
 
         spinContentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCat.setAdapter(spinContentAdapter);
