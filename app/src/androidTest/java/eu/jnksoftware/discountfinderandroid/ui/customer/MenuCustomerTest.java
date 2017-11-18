1c5f6a0b1cf0c839313d6a6f74df3534c047de01
arditdomi <domiardit96@gmail.com>
2017-11-18 19:07:35 +0200

    #103 Closed

---
+++ app/src/androidTest/java/eu/jnksoftware/discountfinderandroid/ui/customer/MenuCustomerTest.java
@@ -0,0 +1,44 @@
+package eu.jnksoftware.discountfinderandroid.ui.customer;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import eu.jnksoftware.discountfinderandroid.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MenuCustomerTest {
+
+    @Rule
+    public ActivityTestRule<MenuCustomer> menuCustomerActivityTestRule = new ActivityTestRule<>(MenuCustomer.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testShopsButtonClick(){
+        onView(withId(R.id.showShopsBtn)).perform(click());
+        onView(withId(R.id.shopsList)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testAboutButtonClick(){
+        onView(withId(R.id.aboutBtn)).perform(click());
+        onView(withText("Copyright Alright Reserved")).check(matches(isDisplayed()));
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
