333cc91fb20a044a60357434dd19fca245e32663
ArisPoz <aris.pozidis96@gmail.com>
2017-11-25 21:41:35 +0200

    commit for issue #116

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/ui/general/Login.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/ui/general/Login.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import org.json.JSONObject;
 import java.util.HashMap;
@@ -19,6 +20,8 @@ public class Login extends Activity {
     private String password;
     EditText etEmail;
     EditText etPassword;
+    ProgressBar loadingBar;
+    TextView loadingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +35,32 @@ public class Login extends Activity {
 
         TextView registerView = findViewById(R.id.loginRegisterBtn);
         registerView.setOnClickListener(registerBtnClick);
+
+        loadingBar = findViewById(R.id.loadingBar);
+        loadingText = findViewById(R.id.loadingText);
     }
 
     private final View.OnClickListener loginBtnClick = new View.OnClickListener() {
         @Override
         public void onClick(final View loginView) {
-          
+
             email = etEmail.getText().toString();
             password = etPassword.getText().toString();
-              
+
             Map<String,String>loginValues=new HashMap<>();
             loginValues.put("username",email);
             loginValues.put("password",password);
 
-            JSONObject sendLogin=new JSONObject(loginValues);
-            LoginApi loginApi=new LoginApi();
-            loginApi.doLogin(Login.this,sendLogin);
 
-        }
+                JSONObject sendLogin = new JSONObject(loginValues);
+                LoginApi loginApi = new LoginApi();
+                loginApi.doLogin(Login.this, sendLogin);
+
+                loadingBar.setVisibility(View.VISIBLE);
+                loadingText.setVisibility(View.VISIBLE);}
+
+
+
     };
     private final View.OnClickListener registerBtnClick = new View.OnClickListener() {
         @Override
