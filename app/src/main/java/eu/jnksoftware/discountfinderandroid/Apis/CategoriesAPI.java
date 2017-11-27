845d3e9a3c036cd6dd8ca23684cd087dd540e377
makis <makis_kovanidis@hotmail.com>
2017-11-28 00:37:02 +0200

    fix bugs

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/Apis/CategoriesAPI.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/Apis/CategoriesAPI.java
@@ -4,6 +4,7 @@ package eu.jnksoftware.discountfinderandroid.Apis;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -22,18 +23,20 @@ import eu.jnksoftware.discountfinderandroid.models.DiscountCategory;
 
 public class CategoriesAPI {
 
-    Context context;
-    ArrayList<DiscountCategory> categories;
+
+    private static final int timeOutInMs = 10000;
+    private static final int numberOfTries = 1;
+    ArrayList<DiscountCategory> categories=new ArrayList<>();
     String categories_url = "http://83.212.117.108:9001/api/category";
 
-    public CategoriesAPI(Context context){
-        this.context = context;
+    public CategoriesAPI(){
     }
 
-    public ArrayList<DiscountCategory> getCategories(){
+    public ArrayList<DiscountCategory> getCategories(final Context context){
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, categories_url
                 , null
                 , new Response.Listener<JSONArray>() {
+                    String message="";
                     @Override
                     public void onResponse(JSONArray response) {
                         int counter=0;
@@ -43,13 +46,14 @@ public class CategoriesAPI {
                                 DiscountCategory category = new DiscountCategory(jsonObject.getString("id")
                                         ,jsonObject.getString("title"));
                                 categories.add(category);
+                                message+="\n"+category.getCategoryTitle();
                                 counter++;
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
 
                         }
-
+                        Toast.makeText(context, "ok\n"+ message, Toast.LENGTH_LONG).show();
 
                     }
                 }
@@ -57,8 +61,9 @@ public class CategoriesAPI {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(context, "Could not fetch categories,check that you have an internet connection", Toast.LENGTH_SHORT).show();
-                    }
+        }
                 });
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(timeOutInMs, numberOfTries, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         Singleton.getmInstance(context).addToRequestQueue(jsonArrayRequest);
 
         return categories;
