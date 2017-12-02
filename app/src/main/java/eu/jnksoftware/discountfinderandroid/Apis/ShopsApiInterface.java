61218b02e53afc6b971d9bb0bd1990e353fdb8af
arditdomi <domiardit96@gmail.com>
2017-12-02 20:17:58 +0200

    commit for issue #115

---
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/Apis/ShopsApiInterface.java
@@ -0,0 +1,28 @@
+package eu.jnksoftware.discountfinderandroid.Apis;
+
+import java.util.List;
+import eu.jnksoftware.discountfinderandroid.models.Shop;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by dito on 12/1/2017.
+ */
+
+public interface ShopsApiInterface {
+
+    @GET("shop")
+    Call<List<Shop>> getShopsList();
+
+    @GET ("shop/")
+    Call<List<Shop>> getShopWithId(@Query("id") int id);
+
+    @GET ("user/shop")
+    Call<List<Shop>> getUserShops();
+
+    @GET ("user/shop/")
+    Call<List<Shop>> getUserShopWithId(@Query("id") int id);
+}
