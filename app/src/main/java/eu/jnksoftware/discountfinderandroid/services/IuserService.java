9730a894685116d277862ad8ced269e1dfd35bdd
Delis Nikos <delisnikos95@gmail.com>
2017-12-03 22:44:00 +0200

    Register with retrofit
    Register with Retrofit library

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/services/IuserService.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/services/IuserService.java
@@ -1,14 +1,16 @@
 package eu.jnksoftware.discountfinderandroid.services;
 
-import eu.jnksoftware.discountfinderandroid.models.DiscountPreferencesPostRequest;
+import eu.jnksoftware.discountfinderandroid.models.DiscountPreferencesPostResponse;
 import eu.jnksoftware.discountfinderandroid.models.DiscountPreferencesRequest;
 import eu.jnksoftware.discountfinderandroid.models.DiscountPreferencesResponse;
+import eu.jnksoftware.discountfinderandroid.models.RegisterTokenRequest;
 import eu.jnksoftware.discountfinderandroid.models.UserTokenRequest;
 import eu.jnksoftware.discountfinderandroid.models.UserTokenResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 
@@ -20,19 +22,23 @@ public interface IuserService {
     @POST("login")
     Call<UserTokenResponse> getTokenAcess(@Body UserTokenRequest userTokenRequest);
 
+    @POST("register")
+    Call<UserTokenResponse> register(@Body RegisterTokenRequest registerTokenRequest);
+
     @PUT("requestDiscount/3")
     Call<DiscountPreferencesResponse> putDiscountPreferences(@Body DiscountPreferencesRequest discountPreferencesRequest);
+    @POST("requestedDiscount")
+    Call<Void>postDiscoutPreferencesHeader(@Header("Authorization")String auth);
+    @POST("requestedDiscount")
+    Call<DiscountPreferencesPostResponse> postDiscountPreferences(@Body DiscountPreferencesRequest discountPreferencesRequest);
 
-    @POST("requestDiscount")
-    Call<DiscountPreferencesRequest> postDiscountPreferences(@Body DiscountPreferencesPostRequest discountPreferencesPostRequest);
-    
-    @GET("requestDiscount/1")
+    @GET("requestedDiscount/1")
     Call<DiscountPreferencesResponse> getOneDiscountPreference();
 
-    @GET("requestDiscount")
+    @GET("requestedDiscount")
     Call<DiscountPreferencesResponse>getDiscountsPreference();
 
-    @DELETE("requestDiscount/3")
+    @DELETE("requestedDiscount/3")
     Call<Void> deleteDiscountPreference();
 
 
