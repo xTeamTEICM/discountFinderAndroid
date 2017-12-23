0f32d484c99360fa2250a9bf5be7acc0f8c5376d
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-12-23 02:36:12 +0200

    #4

--- app/src/main/java/eu/jnksoftware/discountfinderandroid/Utilities/ManageSharePrefs.java
+++ app/src/main/java/eu/jnksoftware/discountfinderandroid/Utilities/ManageSharePrefs.java
@@ -18,26 +18,23 @@ public class ManageSharePrefs {
     private static SharedPreferences mSharedPref;
 
 
-    private ManageSharePrefs()
-    {
+    private ManageSharePrefs() {
 
     }
 
-    public static void init(Context context)
-    {
-        if(mSharedPref == null)
+    public static void init(Context context) {
+        if (mSharedPref == null)
             mSharedPref = context.getSharedPreferences(context.getPackageName(), Activity.MODE_PRIVATE);
     }
 
     public static User readUser(String defValue) {
         User tempUser;
-        if (mSharedPref.contains("userData")){
+        if (mSharedPref.contains("userData")) {
             String userToString = mSharedPref.getString("userData", defValue);
             Gson userJson = new Gson();
             tempUser = userJson.fromJson(userToString, User.class);
-        }
-        else{
-            tempUser=null;
+        } else {
+            tempUser = null;
         }
 
         return tempUser;
@@ -45,27 +42,26 @@ public class ManageSharePrefs {
 
     public static void writeUser(User user) {
         SharedPreferences.Editor prefsEditor = mSharedPref.edit();
-        Gson userJson=new Gson();
+        Gson userJson = new Gson();
         String userString = userJson.toJson(user);
         prefsEditor.putString("userData", userString);
         prefsEditor.commit();
     }
 
-    public static void writeFcmTokenData(String fcmData){
+    public static void writeFcmTokenData(String fcmData) {
         SharedPreferences.Editor prefsEditor = mSharedPref.edit();
         prefsEditor.putString("fcmUSerData", fcmData);
         prefsEditor.commit();
 
     }
 
-    public static String  readFcmTokenData(String defValue){
+    public static String readFcmTokenData(String defValue) {
         String tempFcmData;
-        
-        if (mSharedPref.contains("fcmUserDate")){
+
+        if (mSharedPref.contains("fcmUserDate")) {
             tempFcmData = mSharedPref.getString("fcmUSerData", defValue);
-        }
-        else{
-            tempFcmData=" ";
+        } else {
+            tempFcmData = " ";
         }
 
         return tempFcmData;
